ae1db19b1c2de043180f56ca779df88cac4857d0
Jeffrey Phillips <sportsperson1999@yahoo.com>
2019-03-02 16:43:35 -0600

    Fixed jpg file name

---
+++ scripts/data.js
@@ -0,0 +1,26 @@
+var question0 = {
+  "questionToAsk": "Is this the correct play for default attacking",
+  "imageSource": "../images/std_attack_play.jpg",
+  "answers": [ "True", "False" ],
+  "correctAnswer": 0
+};
+
+var question1 = {
+  "questionToAsk": "Is this the correct play for a backrow attack",
+  "imageSource": "../images/std_attack_play.jpg",
+  "answers": [ "True", "False" ],
+  "correctAnswer": 0
+};
+
+var question2 = {
+  "questionToAsk": "Is this the correct play for trick play #1",
+  "imageSource": "../images/std_attack_play.jpg",
+  "answers": [ "True", "False" ],
+  "correctAnswer": 0
+};
+
+var questions = {
+  "question1": question0,
+  "quesiton2": question1,
+  "question3": question2
+}
