b45d8f9817d43dc303397e3ab8e005ac6db3e9b1
Jeffrey Phillips <sportsperson1999@yahoo.com>
2019-03-02 16:36:18 -0600

    added data.js to html headers

--- scripts/main.js
+++ scripts/main.js
@@ -25,29 +25,3 @@ function submitAnswers() {
   /*document.getElementById("start1").innerHTML = output*/
 }
 
-var question0 = {
-  "questionToAsk": "Is this the correct play for default attacking",
-  "imageSource": "./images/std_attack_plays.jpg",
-  "answers": [ "True", "False" ],
-  "correctAnswer": 0
-};
-
-var question1 = {
-  "questionToAsk": "Is this the correct play for a backrow attack",
-  "imageSource": "./images/std_attack_plays.jpg",
-  "answers": [ "True", "False" ],
-  "correctAnswer": 0
-};
-
-var question2 = {
-  "questionToAsk": "Is this the correct play for trick play #1",
-  "imageSource": "./images/std_attack_plays.jpg",
-  "answers": [ "True", "False" ],
-  "correctAnswer": 0
-};
-
-var questions = {
-  "question1": question0,
-  "quesiton2": question1,
-  "question3": question2
-}
