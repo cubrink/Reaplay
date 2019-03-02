e52124ea8f3a0e68b1ba303091620c6d6a5d8805
Jeffrey Phillips <sportsperson1999@yahoo.com>
2019-03-02 13:49:41 -0600

    updated scripts

--- scripts/main.js
+++ scripts/main.js
@@ -20,3 +20,26 @@ function openNav() {
     x.className = x.className.replace(" w3-show", "");
   }
 }
+
+function loadJSON(callback) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', chrome.extension.getURL('questions.json'), true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+
+ function init() {
+ loadJSON(function(response) {
+  // Parse JSON string into object
+    var actual_JSON = JSON.parse(response);
+    console.log(actual_JSON);
+ });
+}
+
