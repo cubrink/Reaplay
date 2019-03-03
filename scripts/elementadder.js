e442145d99fc8348e0adb8928085e1272bb90b0d
Jeffrey Phillips <sportsperson1999@yahoo.com>
2019-03-03 02:44:05 -0600

    button change

--- scripts/elementadder.js
+++ scripts/elementadder.js
@@ -97,20 +97,24 @@ function add_accordion(reference, position) {
             <button onclick="myFunction('Demo1')" class="w3-button w3-block w3-theme-l1 w3-left-align"><i class="fa fa-circle-o-notch fa-fw w3-margin-right"></i> My Team</button>
 
             <div id="Demo1" class="w3-hide w3-container">
-              <!--<p>MST Club Volleyball</p>-->
-              <button type="button" class="w3-button w3-theme"><i class="fa fa-users"></i>  MST Club Volleyball</button> 
+              <br>
+              <button onclick="window.location.href='./team.html'" type="button" class="w3-button w3-theme"><i class="fa fa-users"></i>  MST Club Volleyball</button>
+              <br><br>
             </div>
 
             <button onclick="myFunction('Demo2')" class="w3-button w3-block w3-theme-l1 w3-left-align"><i class="fa fa-calendar-check-o fa-fw w3-margin-right"></i> My Games</button>
             <div id="Demo2" class="w3-hide w3-container">
+              <br>
               <button type="button" class="w3-button w3-theme"><i class="fa fa-pencil"></i>  Crossover Event</button> 
-              <!--<p>Crossover Event</p>-->
+              <br><br>
             </div>
 
             <button onclick="myFunction('Demo3')" class="w3-button w3-block w3-theme-l1 w3-left-align"><i class="fa fa-video-camera fa-fw w3-margin-right"></i> My Videos</button>
             <div id="Demo3" class="w3-hide w3-container">
           <div class="w3-row-padding">
-          <br>
+              <br>
+              <button type="button" class="w3-button w3-theme"><i class="fa fa-video-camera"></i>  My Videos</button>
+              <br><br>
             <div class="w3-half">
               <video width=100% height=100% controls>
               <source src="./videos/clip1.mp4" type="video/mp4" style="width:100%" class="w3-margin-bottom">
